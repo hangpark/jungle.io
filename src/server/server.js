f8c6a67f9825433330f9aeaed6faefb1e90856ad
jhjerry <jhjerry97@gmail.com>
2017-07-15 03:17:25 +0900

    changed tap length 2->4

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,9 @@ var express = require('express');
 var app     = express();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
-var SAT = require('sat'); //기하 처리용
+
+var SAT = require('sat'); //충돌 처리
+var V = SAT.Vector;
 
 var config  = require('./config.json');
 var util = require('./util');
@@ -22,83 +24,80 @@ function getAllEntities() {
 }
 
 var sockets = {};
+
 var leaderboard = [];
 var leaderboardChanged = false;
 
-
-
 io.on('connection', function (socket) {
-  console.log("Somebody connected!", socket.handshake.query.type);
+    console.log("Somebody connected!", socket.handshake.query.type);
 
-  var type = socket.handshake.query.type;
-  var position = util.randomPosition();
+    var type = socket.handshake.query.type;
+    var position = util.randomPosition();
 
-  var currentPlayer = {
-    id: socket.id,
-  };
+    var currentPlayer = {
+        id: socket.id,
+    };
 
-  socket.on('respawn', function () {
+    socket.on('respawn', function () {
     //우선 players 목록에 존재하면 삭제하고 나중에('gotit')에서 다시 추가함
-    if (util.findIndex(players, currentPlayer.id) > -1)
-      players.splice(util.findIndex(players, currentPlayer.id), 1);
+        if (util.findIndex(players, currentPlayer.id) > -1)
+            players.splice(util.findIndex(players, currentPlayer.id), 1);
 
-    socket.emit('welcome', currentPlayer);
+        socket.emit('welcome', currentPlayer);
 
-    if(currentPlayer.name == undefined)
-      console.log('new player tries to join the game');
-    else
-      console.log(currentPlayer.name + ' respawns');
-  });
+        if(currentPlayer.name == undefined)
+            console.log('new player tries to join the game');
+        else
+            console.log(currentPlayer.name + ' respawns');
+    });
 
-  socket.on('gotit', function (player) {
-    console.log('[INFO] Player' + player.name + ' connected');
-    sockets[player.id] = socket;
+    socket.on('gotit', function (player) {
+        console.log('[INFO] Player' + player.name + ' connected');
+        sockets[player.id] = socket;
 
-    player.x = position.x;
-    player.y = position.y;
-    player.direction = 0;
-    player.speed = 0;
-    player.score = 0;
-    currentPlayer = player;
-    players.push(currentPlayer);
+        player.x = position.x;
+        player.y = position.y;
+        player.direction = 0;
+        player.speed = 0;
+        player.score = 0;
+        currentPlayer = player;
+        players.push(currentPlayer);
 
-    socket.emit('gameSetup', {
-      gameWidth: config.gameWidth,
-      gameHeight: config.gameHeight
+        socket.emit('gameSetup', {
+            gameWidth: config.gameWidth,
+            gameHeight: config.gameHeight
+        });
+    });
+
+    socket.on('windowResized', function(data) {
+        currentPlayer.screenWidth = data.screenWidth;
+        currentPlayer.screenHeight = data.screenHeight;
     });
 
 
-  });
+    socket.on('playerSendDirection', function(direction) {
+        if(direction !== currentPlayer.direction) {
+            currentPlayer.direction = direction;
+        }
+    });
 
-  socket.on('windowResize', function(data) {
-    currentPlayer.screenWidth = data.screenWidth;
-    currentPlayer.screenHeight = data.screenHeight;
-  });
+    socket.on('playerSendSpeed', function(speed) {
+        if(speed !== currentPlayer.speed) {
+            currentPlayer.speed = speed;
+        }
+    });
 
-
-  socket.on('playerSendDirection', function(direction) {
-    if(direction !== currentPlayer.direction) {
-      currentPlayer.direction = direction;
-    }
-  });
-
-  socket.on('playerSendSpeed', function(speed) {
-    if(speed !== currentPlayer.speed) {
-      currentPlayer.speed = speed;
-    }
-  });
-
-  socket.on('playerSendAttack', function(attack) {
+    socket.on('playerSendAttack', function(attack) {
     //
-  });
+    });
 
 });
 
 
 function movePlayer(player) {
-  //canvas에서 각 어떻게 처리하는지 고려
-  player.x += player.speed * Math.sin(player.direction);
-  player.y -= player.speed * Math.sin(player.direction);
+    //canvas에서 각 어떻게 처리하는지 고려
+    player.x += player.speed * Math.sin(player.direction);
+    player.y -= player.speed * Math.sin(player.direction);
 
 }
 
@@ -106,48 +105,68 @@ function movePlayer(player) {
 
 
 function tickPlayer(currentPlayer) {
-  movePlayer(currentPlayer);
+    movePlayer(currentPlayer);
 
   //공격 확인, 죽음 이벤트 발생('serverTellPlayerDie') ....
 }
 
 
 function moveloop() {
-  for (var i = 0; i < players.length; i++) {
-    tickPlayer(players[i]);
-  }
-  for (i = 0; i < ais.length; i++) {
-    tickPlayer(ais[i]);
-  }
+    for (var i = 0; i < players.length; i++) {
+        tickPlayer(players[i]);
+    }
+    for (i = 0; i < ais.length; i++) {
+        tickPlayer(ais[i]);
+    }
 }
 
 function gameloop() {
-  if(players.lenght > 0) {
-    players.sort( function(a,b) { return b.score - a.score; });
-  }
-  var topPlayers = [];
-  //leaderboard 관련 동작들....
+    if (players.lenght > 0) {
+        players.sort( function(a,b) { return b.score - a.score; });
+    }
+    var topPlayers = [];
+    //leaderboard 관련 동작들....
+    for (var i = 0; i < Math.min(5, players.length); i++) {
+        topPlayers.push({
+            id: players[i].id,
+            name: players[i].name
+        });
+    }
+    if(leaderboard.length !== topPlayers.length) {
+        leaderboard = topPlayers;
+        leaderboardChanged = true;
+    } else {
+        for(i = 0; i < leaderboard.length; i++) {
+            if(leaderboard[i].id !== topPlayers[i].id) {
+                leaderboard = topPlayers;
+                leaderboardChanged = true;
+                break;
+            }
+        }
+    }
 
 
 }
 
 
 function sendUpdates() {
-  var entities = getAllEntities();
-  entities.forEach( function(e) {
-    var visibleEntities = entities.filter(); //계산법?
+    var entities = getAllEntities();
+    players.forEach( function(e) {
+        var visibleEntities = entities.filter(); //계산법?
+        var visibleAttacks = [];
+        var visibleBloods = [];
 
-    if(e.type === 'player') {
-      sockets[e.id].emit('serverTellPlayerMove', visibleEntities, visibleAttacks, visibleBloods);
-      if(leaderboardChanged) {
-        sockets[e.id].emit('leaderboard', {
-          players: players.length,
-          leaderboard: leaderboard
-        });
-      }
-    }
-  });
-  leaderboardChanged = false;
+        if(e.type === 'player') {
+            sockets[e.id].emit('serverTellPlayerMove', visibleEntities, visibleAttacks, visibleBloods);
+            if(leaderboardChanged) {
+                sockets[e.id].emit('leaderboard', {
+                    players: players.length,
+                    leaderboard: leaderboard
+            });
+          }
+        }
+    });
+    leaderboardChanged = false;
 }
 
 
@@ -161,5 +180,5 @@ setInterval(sendUpdates, 1000 / config.networkUpdateFactor);
 
 var serverPort = process.env.PORT || config.port;
 http.listen(serverPort, function() {
-  console.log("Server is listening on port " + serverPort);
+    console.log("Server is listening on port " + serverPort);
 });
--- src/server/util.js
+++ src/server/util.js
@@ -5,22 +5,22 @@
 var config = require('./config.json');
 
 exports.randomInRange = function (from, to) {
-  return Math.floor(Math.random() * (to -from)) + from;
+    return Math.floor(Math.random() * (to -from)) + from;
 };
 
 exports.randomPosition = function () {
-  return {
-    x: exports.randomInRange(config.playerSize, config.gameWidth - config.playerSize),
-    y: exports.randomInRange(config.playerSize, config.gameHeight - config.playerSize)
-  };
+    return {
+        x: exports.randomInRange(config.playerSize, config.gameWidth - config.playerSize),
+        y: exports.randomInRange(config.playerSize, config.gameHeight - config.playerSize)
+    };
 };
 
 exports.findIndex = function(arr, id) {
-  var len = arr.length;
-  while(len--) {
-    if(arr[len].id === id) {
-      return len;
+    var len = arr.length;
+    while(len--) {
+        if(arr[len].id === id) {
+            return len;
+        }
     }
-  }
-  return -1;
+    return -1;
 };
