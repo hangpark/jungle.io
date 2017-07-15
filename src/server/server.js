87e5febd138ed8e46a52fb9243c896b01c7a8568
jhjerry <jhjerry97@gmail.com>
2017-07-15 23:24:25 +0900

    basic game logics based on SAT.js
    -check attacks
    -filter visible entities
    
    -new idea on ai: periodically set its destination in game field randomly

--- src/server/server.js
+++ src/server/server.js
@@ -8,8 +8,10 @@ var io      = require('socket.io')(http);
 
 var SAT = require('sat'); //충돌 처리
 var V = SAT.Vector;
+var C = SAT.Circle;
+var P = SAT.Polygon;
 
-var config  = require('./config.json');
+var cfg  = require('./config.json');
 var util = require('./util');
 
 app.use(express.static(__dirname + '/../client'));
@@ -40,12 +42,14 @@ io.on('connection', function (socket) {
         id: socket.id,
     };
 
-    socket.on('respawn', function () {
+    socket.on('respawn', function (screenWidth, screenHeight) {
     //우선 players 목록에 존재하면 삭제하고 나중에('gotit')에서 다시 추가함
         if (util.findIndex(players, currentPlayer.id) > -1)
             players.splice(util.findIndex(players, currentPlayer.id), 1);
+        currentPlayer.screenWidth = screenWidth;
+        currentPlayer.screenHeight = screenHeight;
 
-        socket.emit('welcome', currentPlayer);
+        socket.emit('welcome', currentPlayer, cfg.gameWidth, cfg.gameHeight);
 
         if(currentPlayer.name == undefined)
             console.log('new player tries to join the game');
@@ -66,14 +70,18 @@ io.on('connection', function (socket) {
         players.push(currentPlayer);
 
         socket.emit('gameSetup', {
-            gameWidth: config.gameWidth,
-            gameHeight: config.gameHeight
+            gameWidth: cfg.gameWidth,
+            gameHeight: cfg.gameHeight
         });
     });
 
     socket.on('windowResized', function(data) {
         currentPlayer.screenWidth = data.screenWidth;
         currentPlayer.screenHeight = data.screenHeight;
+    });
+
+    socket.on('playerSendStatus', function (status) {
+
     });
 
 
@@ -89,8 +97,13 @@ io.on('connection', function (socket) {
         }
     });
 
-    socket.on('playerSendAttack', function(attack) {
-        attack.duration = config.attackDuration;
+    socket.on('playerSendAttack', function() {
+        var attack = {
+            attacker: currentPlayer,
+            duration: cfg.attackDuration,
+            x: currentPlayer.x + cfg.playerSize * Math.sin(currentPlayer.direction),
+            y: currentPlayer.y - cfg.playerSize * Math.cos(currentPlayer.direction),
+        };
         attacks.push(attack);
     });
 
@@ -100,7 +113,7 @@ io.on('connection', function (socket) {
 function movePlayer(player) {
     //canvas에서 각 어떻게 처리하는지 고려
     player.x += player.speed * Math.sin(player.direction);
-    player.y -= player.speed * Math.sin(player.direction);
+    player.y -= player.speed * Math.cos(player.direction);
 
 }
 
@@ -110,23 +123,58 @@ function movePlayer(player) {
 function tickPlayer(currentPlayer) {
     movePlayer(currentPlayer);
 
-  //공격 확인, 죽음 이벤트 발생('serverTellPlayerDie') ....
-  io.emit('serverTellPlayerDie', function(death) {
+    //TODO player's shape?
+    var playerArea = new P(new V(currentPlayer.x, currentPlayer.y), [
+        new V(0, -cfg.playerSize),
+        new V(cfg.playerSize / 2, cfg.playerSize / 2),
+        new V(-cfg.playerSize / 2, cfg.playerSize / 2),
+    ]);
 
-  });
+    for (var attack in attacks) {
+        if((util.distance(currentPlayer.x, currentPlayer.y, attack.x, attack.y) > cfg.attackRadius + cfg.playerSize) ||
+            (attack.attacker == currentPlayer))  continue; //check if the attack is valid.
+
+        var attackCircle = new C(new V(attack.x, attack.y), cfg.attackRadius);
+        var attacked = SAT.testPolygonCircle(playerArea, attackCircle, new SAT.Response());
+        if (attacked) {
+            bloods.push({
+                x: currentPlayer.x,
+                y: currentPlayer.y,
+                duration: cfg.bloodDuration,
+            });
+
+            if(currentPlayer.type === 'human') {
+                sockets[currentPlayer.id].emit('serverTellPlayerDie');
+                players.splice(util.findIndex(players, currentPlayer), 1);
+                attack.attacker.score++;
+            } else {
+                attack.attacker.score -= 2;
+                ais.splice(util.findIndex(ais, currentPlayer), 1);
+            }
+
+            break;
+        }
+    }
+
+    //공격 확인, 죽음 이벤트 발생('serverTellPlayerDie') ....
+
 }
 
 //ai ->speed: 0 or 1, 방향, 속도 유지 시간 지정, 적절한 범위 내에서 방향과 속도를 바꿀 것
+//ai 작동 방식 -> 그냥 사람처럼 움직이는 게 아니라 일정주기마다 목적지를 정해서 그쪽으로 걸어가게 하자?
 function updateAiState(ai) {
-    if(ai.duration === 0) {
-        //방향 변화
-
-        //속도 변화
-        ai.speed = Math.floor(Math.random() + 0.5);
-    } else {
-        (ai.duration)--;
-    }
-
+    // if (util.isInBoundary(ai)) {
+    //
+    // } else if (ai.duration === 0) {
+    //     //방향 변화
+    //
+    //     //속도 변화
+    //     ai.speed = Math.floor(Math.random() + 0.5);
+    // }
+    // else {
+    //     (ai.duration)--;
+    // }
+    //
 
 }
 
@@ -193,19 +241,32 @@ function gameloop() {
 function sendUpdates() {
     var entities = getAllEntities();
     players.forEach( function(p) {
-        var visibleEntities = entities.filter(); //계산법?
-        var visibleAttacks = [];
-        var visibleBloods = [];
+        var screenBox = new P( new V(p.x, p.y), [
+            new V(p.screenWidth / 2, cfg.backSight),
+            new V(- p.screenWidth / 2, cfg.backSight),
+            new V(- p.screenWidth / 2, cfg.backSight - p.screenHeight),
+            new V(p.screenWidth / 2, cfg.backSight - p.screenHeight)
+        ]).rotate(p.direction);
 
-        if(p.type === 'player') {
-            sockets[p.id].emit('serverTellPlayerMove', visibleEntities, visibleAttacks, visibleBloods);
-            if(leaderboardChanged) {
-                sockets[p.id].emit('leaderboard', {
-                    players: players.length,
-                    leaderboard: leaderboard
-            });
-          }
-        }
+        var visibleEntities = entities.filter( function (e) {
+            return SAT.pointInPolygon(new V(e.x, e.y), screenBox);
+        });
+
+        var visibleAttacks = attacks.filter( function (a) {
+            return SAT.pointInPolygon(new V(a.x, a.y), screenBox);
+        });
+        var visibleBloods = bloods.filter( function (b) {
+            return SAT.pointInPolygon(new V(b.x, b.y), screenBox);
+        });
+
+        sockets[p.id].emit('serverTellPlayerMove', visibleEntities, visibleAttacks, visibleBloods);
+        if(leaderboardChanged) {
+            sockets[p.id].emit('leaderboard', {
+                players: players.length,
+                leaderboard: leaderboard
+        });
+      }
+
     });
     leaderboardChanged = false;
 }
@@ -213,13 +274,10 @@ function sendUpdates() {
 
 setInterval(moveloop, 1000 / 60);
 setInterval(gameloop, 1000);
-setInterval(sendUpdates, 1000 / config.networkUpdateFactor);
+setInterval(sendUpdates, 1000 / cfg.networkUpdateFactor);
 
 
-
-
-
-var serverPort = process.env.PORT || config.port;
+var serverPort = process.env.PORT || cfg.port;
 http.listen(serverPort, function() {
     console.log("Server is listening on port " + serverPort);
 });
--- src/server/util.js
+++ src/server/util.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-var config = require('./config.json');
+var cfg = require('./config.json');
 
 exports.validNick = function(nickname) {
     var regex = /^\w*$/;
@@ -15,10 +15,11 @@ exports.randomInRange = function (from, to) {
 
 exports.randomPosition = function () {
     return {
-        x: exports.randomInRange(config.playerSize, config.gameWidth - config.playerSize),
-        y: exports.randomInRange(config.playerSize, config.gameHeight - config.playerSize)
+        x: exports.randomInRange(cfg.playerSize, cfg.gameWidth - cfg.playerSize),
+        y: exports.randomInRange(cfg.playerSize, cfg.gameHeight - cfg.playerSize)
     };
 };
+
 
 exports.findIndex = function(arr, id) {
     var len = arr.length;
@@ -28,4 +29,13 @@ exports.findIndex = function(arr, id) {
         }
     }
     return -1;
+};
+
+exports.distance = function (x1, y1, x2, y2) {
+    return Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+};
+
+
+exports.isInBoundary = function (entity) {
+    return ( ||);
 };
